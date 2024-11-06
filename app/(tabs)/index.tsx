50e51604f8455274855168be1fe97270e5fa9300
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2024-11-06 10:55:34 -0500

    fire base

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,41 @@
+import { getApp } from "firebase/app";
+import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { app } from "@/app/init";
+
 import { Image, StyleSheet, Platform, Button } from 'react-native';
 
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { Redirect, useRouter } from 'expo-router';
 import CanDoScrollView from '@/components/CanDoScrollView';
 import LoremIpsumGenerator from '@/components/LoremIpsum';
+import { useEffect } from "react";
 
 // A.K.A. Task List
 
+const db = getFirestore(app);
+
 export default function TasksScreen() {
   const router = useRouter();
+
   return (
     <CanDoScrollView>
       <ThemedText>
-        <Button title="Go to Details" onPress={() => {
-          router.push('/(tabs)/calendar');
+        <Button title="Add New Thing To the DB" onPress={() => {
+          addDoc(collection(db, "Tasks"), {
+            category: "yup",
+            completed: true,
+            date: "sometime",
+            description: "yeah",
+            frequency: "what",
+            groupName: "mhm",
+            notification: "yeag",
+            points: 10000000000,
+            time: "eventually",
+            title: "king"
+          }).then(() => {
+            console.log("the deed is done")
+          });
         }}>
 
         </Button>
---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDfSKYfm_ag6TiUj0bMrpku5TOpMDHxESM",
+    authDomain: "cando-68065.firebaseapp.com",
+    projectId: "cando-68065",
+    storageBucket: "cando-68065.firebasestorage.app",
+    messagingSenderId: "739196734453",
+    appId: "1:739196734453:web:637c5a2a183da7ad126ebc",
+    measurementId: "G-55PDK0BYN3"
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
