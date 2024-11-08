af4b9223b2160295881e5f0e60d88a0c6abc3941
pabbatitanvi <pabbatitanvi@gmail.com>
2024-11-08 00:28:32 -0500

    added settings and initial calendar screen

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, TouchableOpacity, Alert, Text } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -10,31 +10,128 @@ import LoremIpsumGenerator from '@/components/LoremIpsum';
 // A.K.A. Task List
 
 export default function CalendarScreen() {
+    const handleJan = () => {
+        Alert.alert('January Clicked')
+    }
+    const handleFeb = () => {
+        Alert.alert('February Clicked')
+    }
+    const handleMar = () => {
+        Alert.alert('March Clicked')
+    }
+    const handleApr = () => {
+        Alert.alert('April Clicked')
+    }
+    const handleMay = () => {
+        Alert.alert('May Clicked')
+    }
+    const handleJun = () => {
+        Alert.alert('June Clicked')
+    }
+    const handleJul = () => {
+        Alert.alert('July Clicked')
+    }
+    const handleAug = () => {
+        Alert.alert('August Clicked')
+    }
+    const handleSep = () => {
+        Alert.alert('September Clicked')
+    }
+    const handleOct = () => {
+       Alert.alert('October Clicked')
+    }
+    const handleNov = () => {
+        Alert.alert('November Clicked')
+    }
+    const handleDec = () => {
+       Alert.alert('December Clicked')
+    }
+
     return (
       <CanDoScrollView>
-        <ThemedText>
-            <LoremIpsumGenerator paragraphs={50} />
-        </ThemedText>
+        <View style = {styles.container}>
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.button} onPress = {handleJan}>
+                    <Text style = {styles.buttonText}>January</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.button} onPress = {handleFeb}>
+                    <Text style = {styles.buttonText}>February</Text>
+                </TouchableOpacity>
+            </View>
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.button} onPress = {handleMar}>
+                    <Text style = {styles.buttonText}>March</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.button} onPress = {handleApr}>
+                   <Text style = {styles.buttonText}>April</Text>
+                </TouchableOpacity>
+            </View>
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.button} onPress = {handleMay}>
+                    <Text style = {styles.buttonText}>May</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.button} onPress = {handleJun}>
+                    <Text style = {styles.buttonText}>June</Text>
+                </TouchableOpacity>
+            </View>
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.button} onPress = {handleJul}>
+                    <Text style = {styles.buttonText}>July</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.button} onPress = {handleAug}>
+                    <Text style = {styles.buttonText}>August</Text>
+                </TouchableOpacity>
+            </View>
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.button} onPress = {handleSep}>
+                    <Text style = {styles.buttonText}>September</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.button} onPress = {handleOct}>
+                    <Text style = {styles.buttonText}>October</Text>
+                </TouchableOpacity>
+            </View>
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.button} onPress = {handleNov}>
+                    <Text style = {styles.buttonText}>November</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.button} onPress = {handleDec}>
+                    <Text style = {styles.buttonText}>December</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
       </CanDoScrollView>
     );
   }
   
   const styles = StyleSheet.create({
-    titleContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: 8,
+    container: {
+        flex: 1,
+        justifyContent: 'center',
     },
-    stepContainer: {
-      gap: 8,
-      marginBottom: 8,
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 15
     },
-    reactLogo: {
-      height: 178,
-      width: 290,
-      bottom: 0,
-      left: 0,
-      position: 'absolute',
+    button: {
+        borderWidth: 2,
+        height: 50,
+        width: 150,
+        borderColor: 'white',
+        borderRadius: 7,
+        alignItems: 'center',
+        justifyContent: 'center'
+
     },
+    buttonText: {
+        color: 'white',
+        fontSize: 20
+    }
   });
   
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,34 +1,140 @@
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View, Button, Alert, TouchableOpacity} from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
+import { Picker } from '@react-native-picker/picker';
 import CanDoScrollView from '@/components/CanDoScrollView';
+import { useState } from 'react';
+import { useColorScheme } from 'react-native'
+import { Colors } from '@/constants/Colors';
 
-export default function SettingsScreen() {
+export default function SettingsScreen(){
+  const colorScheme = useColorScheme() ?? 'light';
+  const tint = colorScheme === 'dark' ? Colors.dark.tint : Colors.light.tint;
+  const [theme, setTheme] = useState("dark");
+  const [size, setSize] = useState("regular");
+  const [startDay, setStartDay] = useState("monday");
+  const [infoDisplay, setInfoDisplay] = useState("location");
+  const [exportFormat, setExportFormat] = useState("appleCalendar");
+  const handleEditAccount = () =>{
+    Alert.alert('Edit account details clicked')
+  }
+
+  const handleDeleteAccount = () =>{
+    Alert.alert('Delete account clicked')
+  }
+
   return (
     <CanDoScrollView>
-      <ThemedText>
-        Hi.
-      </ThemedText>
+      <View style = {styles.container}>
+        <Text style = {styles.centerText}>Appearance</Text>
+
+        <View style={styles.row}>
+            <ThemedText style={styles.settingsName}>Theme</ThemedText>
+            <Picker selectedValue={theme} style={styles.dropdown} dropdownIconColor={tint} itemStyle={styles.dropdownItem} onValueChange={setTheme}>
+                <Picker.Item label="Dark" value="dark" />
+                <Picker.Item label="Light" value="light" />
+            </Picker>
+        </View>
+
+        <View style={styles.row}>
+            <ThemedText style={styles.settingsName}>Font Size</ThemedText>
+                <Picker selectedValue={size} style={styles.dropdown} dropdownIconColor={tint} itemStyle={styles.dropdownItem} onValueChange={setSize}>
+                    <Picker.Item label="Small" value="small" />
+                    <Picker.Item label="Regular" value="regular" />
+                    <Picker.Item label="Large" value="large" />
+                </Picker>
+        </View>
+
+        <Text style = {styles.centerText}>Calendar</Text>
+
+        <View style={styles.row}>
+            <ThemedText style={styles.settingsName}>Week Start Day</ThemedText>
+                <Picker selectedValue={startDay} style={styles.dropdown} dropdownIconColor={tint} itemStyle={styles.dropdownItem} onValueChange={setStartDay}>
+                    <Picker.Item label="Sunday" value="sunday" />
+                    <Picker.Item label="Monday" value="Monday" />
+                    <Picker.Item label="Saturday" value="saturday" />
+                </Picker>
+        </View>
+
+        <View style={styles.row}>
+            <ThemedText style={styles.settingsName}>Info Display</ThemedText>
+                <Picker selectedValue={infoDisplay} style={styles.dropdown} dropdownIconColor={tint} itemStyle={styles.dropdownItem} onValueChange={setInfoDisplay}>
+                    <Picker.Item label="Location" value="location" />
+                    <Picker.Item label="Due Date" value="dueDate" />
+                    <Picker.Item label="Description" value="description" />
+                </Picker>
+        </View>
+
+        <View style={styles.row}>
+            <ThemedText style={styles.settingsName}>Export Format</ThemedText>
+                <Picker selectedValue={exportFormat} style={styles.dropdown} dropdownIconColor={tint} itemStyle={styles.dropdownItem} onValueChange={setExportFormat}>
+                    <Picker.Item label="Outlook" value="outlook" />
+                    <Picker.Item label="Apple Calendar" value="appleCalendar" />
+                    <Picker.Item label="Google Calendar" value="googleCalendar" />
+                </Picker>
+        </View>
+
+        <Text style = {styles.centerText}>Account</Text>
+
+        <View style = {styles.buttonRow}>
+            <TouchableOpacity style = {{backgroundColor: 'grey', borderRadius:8, marginVertical: 20}} onPress = {handleEditAccount}>
+                <Text style = {styles.buttonText}> Edit Account Details </Text>
+            </TouchableOpacity>
+        </View>
+
+        <View style = {styles.buttonRow}>
+            <TouchableOpacity style = {{backgroundColor: 'red', borderRadius: 8}} onPress = {handleDeleteAccount}>
+                <Text style = {styles.buttonText}> Delete Account </Text>
+            </TouchableOpacity>
+        </View>
+      </View>
     </CanDoScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+
+  centerText: {
+    textAlign: 'center',
+    fontSize: 32,
+    color: 'white',
+    marginVertical: 0
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 0
+  },
+  row: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    marginVertical: 0
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  dropdown: {
+    color: 'white',
+    flexShrink: 1,
+    flexGrow: 1,
+    height: 90
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  dropdownItem: {
+    color: 'white',
+    fontSize: 20,
+    height: 90
   },
+  settingsName: {
+    marginRight: 10,
+    color: 'white',
+    fontSize: 20
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
 });
