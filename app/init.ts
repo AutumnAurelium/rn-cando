0a9e206ccca748a5ce8caa2ee9661c276be245ce
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2024-11-18 11:08:15 -0500

    native code integration pt. 1

--- app/init.ts
+++ app/init.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
 // import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
